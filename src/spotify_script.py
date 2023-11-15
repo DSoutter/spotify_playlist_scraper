33305e0fc2a2fd2fe1ac47a39ad070c0f0e81f7a
DSoutter <duncan.soutter@gmail.com>
2023-11-15 15:34:52 +0000

    Added logic

--- src/spotify_script.py
+++ src/spotify_script.py
@@ -7,12 +7,16 @@ import csv
 # Ask user if they have a path and paste if so
 file_exists = input("Do you have a spotify_credentials.py file? (Y/N): ")
 if file_exists == "Y":
-    file_path = input("Please enter your file path to this file: ")
-    client_id = 'from file to be added'
-    print("Client_ID is: " + client_id)
+    try:
+        import spotify_credentials as creds
+        client_id = creds.CLIENT_ID
+        print("Client_ID is: " + client_id)
 
-    client_secret = 'from file to be added'
-    print("Client_Secret is: " + client_secret)
+        client_secret = creds.CLIENT_SECRET
+        print("Client_Secret is: " + client_secret)
+    except:
+        print('Check the file is called "spotify_credentials.py" and is stored in the src folder')
+
 else:
     client_id = input("Enter Client_ID: ")
     print("Client_ID is: " + client_id)
@@ -20,7 +24,5 @@ else:
     client_secret = input("Enter Client_Secret: ")
     print("Client_Secret is: " + client_secret)
   
-#if os.path.isfile(/Users/duncan/Documents/Programming/spotify_playlist_scraper/src)
-
 redirect_uri = 'https://localhost:3000'
 print (redirect_uri)
