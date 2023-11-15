e7537f4e6965fbf2c8300116ee4e18cfaf53b7c6
DSoutter <duncan.soutter@gmail.com>
2023-11-15 15:40:17 +0000

    Tweaked try except

--- src/spotify_script.py
+++ src/spotify_script.py
@@ -6,16 +6,18 @@ import csv
 # Check if there's a spotify_credentials.py file
 # Ask user if they have a path and paste if so
 file_exists = input("Do you have a spotify_credentials.py file? (Y/N): ")
-if file_exists == "Y":
+if file_exists.upper() == "Y":
     try:
-        import spotify_credentials as creds
+        import spotify_credenntials as creds
         client_id = creds.CLIENT_ID
         print("Client_ID is: " + client_id)
 
         client_secret = creds.CLIENT_SECRET
         print("Client_Secret is: " + client_secret)
     except:
-        print('Check the file is called "spotify_credentials.py" and is stored in the src folder')
+        print('Check the file is called "spotify_credentials.py" and is stored in the src folder.')
+        print('There should be two variables "CLIENT_ID" and "CLIENT_SECRET"')
+
 
 else:
     client_id = input("Enter Client_ID: ")
