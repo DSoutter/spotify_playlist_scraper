3dcf7ac357e7480a0ec9d733c703d4daf6668844
DSoutter <duncan.soutter@gmail.com>
2023-11-18 20:58:56 +0000

    Tidying up a bit

--- src/playlist_logic.py
+++ src/playlist_logic.py
@@ -5,13 +5,16 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 auth_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(auth_manager=auth_manager)
-user = sp.user('dsoutter')
-playlists = sp.user_playlists('dsoutter')
-get_playlists = print(user["display_name"])
-# while playlists:
-#     for i, playlist in enumerate(playlists['items']):
-#         print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
-#     if playlists['next']:
-#         playlists = sp.next(playlists)
-#     else:
-#         playlists = None
+
+user = sp.user("dsoutter")
+def get_playlists():
+    print(user["display_name"])
+
+
+    # while playlists:
+    #     for i, playlist in enumerate(playlists['items']):
+    #         print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
+    #     if playlists['next']:
+    #         playlists = sp.next(playlists)
+    #     else:
+    #         playlists = None
--- src/spotify_script.py
+++ src/spotify_script.py
@@ -2,29 +2,23 @@ import os.path
 import playlist_logic as pl
 
 # Check if there's a spotify_credentials.py file
-# Ask user if they have a path and paste if so
 file_exists = input("Do you have a spotify_credentials.py file? (Y/N): ")
+while file_exists.upper() not in ("Y", "N"):
+    file_exists = input("You did not enter \"Y\" or \"N\", try again: ")
+
 if file_exists.upper() == "Y":
     try:
         import spotify_credentials as creds
         SPOTIPY_CLIENT_ID = creds.CLIENT_ID
-        print("Client_ID is: " + SPOTIPY_CLIENT_ID)
-
         SPOTIPY_CLIENT_SECRET = creds.CLIENT_SECRET
-        print("Client_Secret is: " + SPOTIPY_CLIENT_SECRET)
     except:
-        print('Check the file is called "spotify_credentials.py" and is stored in the src folder.')
-        print('There should be two variables "CLIENT_ID" and "CLIENT_SECRET"')
+        print("Check the file is called \"spotify_credentials.py\" and is stored in the src folder.")
+        print("There should be two variables \"CLIENT_ID\" and \"CLIENT_SECRET\"")
 
-
-else:
+elif file_exists.upper() == "N":
     SPOTIPY_CLIENT_ID = input("Enter Client_ID: ")
-    print("Client_ID is: " + SPOTIPY_CLIENT_ID)
-
     SPOTIPY_CLIENT_SECRET = input("Enter Client_Secret: ")
-    print("Client_Secret is: " + SPOTIPY_CLIENT_SECRET)
-  
-SPOTIPY_REDIRECT_URI = 'https://localhost:3000'
-print (SPOTIPY_REDIRECT_URI)
+
+SPOTIPY_REDIRECT_URI = "https://localhost:3000"
 
 pl.get_playlists()
