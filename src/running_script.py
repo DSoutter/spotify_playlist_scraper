e649be607bab0f6e99579a14ab991b6cc6828637
DSoutter <duncan.soutter@gmail.com>
2023-12-16 15:09:53 +0000

    starting tracks_logic

--- src/running_script.py
+++ src/running_script.py
@@ -1,7 +1,7 @@
 import setup_script
 import playlist_logic as pl
+import tracks_logis as tl
 
-
-pl.write_to_csv()
-
+#pl.write_to_csv()
+tl.write_to_csv()
 # Next read CSV and write a new csv for the tracks in a playlist
--- src/tracks_logic.py
+++ src/tracks_logic.py
@@ -2,18 +2,19 @@ import os.path
 from setup_script import sp_oauth as sp 
 import csv
 current_user_id = sp.current_user()["id"]
-csv_header = ["Playlist Name", "Number of Songs", "Playlist Id"]
+csv_header = ["Playlist Name", "Song Name", "Artist/Artists", "Album", "Album release date", "Song Popularity"]
+playlist_id = '4dOQIqHSfcmFXtPm79lMNn'
 
-
-def get_playlists(writer, offset):
+def get_songs(writer, offset):
     user_playlists = sp.current_user_playlists(50, offset)["items"]
 
     for item in user_playlists:
         if item["owner"]["id"] == current_user_id:
             writer.writerow([item["name"], str(item["tracks"]["total"]), item["uri"][17:39]])
+            print(item)
 
 def write_to_csv():
-    with open('/Users/duncan/Downloads/playlists.csv', 'w') as f:
+    with open('/Users/duncan/Downloads/playlist_songs.csv', 'w') as f:
         # create the csv writer
         writer = csv.writer(f)
         # write a header to the csv file
