96b5977d1706c4a24a76e918096caf9d4a1ed026
DSoutter <duncan.soutter@gmail.com>
2024-01-07 17:31:28 +0000

    Working e2e for 1 playlist

--- src/tracks_logic.py
+++ src/tracks_logic.py
@@ -3,36 +3,31 @@ from setup_script import sp_oauth as sp
 import csv
 current_user_id = sp.current_user()["id"]
 csv_header = ["Playlist Name", "Song Name", "Song Popularity", "Artist/Artists", "Album", "Album release date"]
-playlist_id = "4dOQIqHSfcmFXtPm79lMNn"
+playlist_id = "6xjb4RLYCxd1rEQD5g6LGv"
 
 def get_tracks(writer, offset):
-    playlist_tracks = sp.current_user_playlists(50, offset)["items"]
+    pl_tracks = sp.playlist_items(playlist_id)["items"]
 
-    for item in user_playlists:
-        if item["owner"]["id"] == current_user_id:
-            writer.writerow([item["name"], str(item["tracks"]["total"]), item["uri"][17:39]])
-            print(item)
+    for track in pl_tracks:
+        # For multiple artists, it needs to loop but only add a comma if there is more than one. 
+        artists = ""
+        for artist in track["track"]["artists"]:
+            if artists:
+                artists += ", "
+            artists += artist["name"]    
+
+        writer.writerow([sp.playlist(playlist_id)["name"],
+                         track["track"]["name"],
+                         track["track"]["popularity"],
+                         artists,
+                         track["track"]["album"]["name"],
+                         track["track"]["album"]["release_date"]])
 
 def write_to_csv():
-    print("Playlist Name:")
-    print(sp.playlist(playlist_id)["name"])
-    print("Song Name:")
-    print(sp.playlist_items(playlist_id)["items"][0]["track"]["name"])
-    print("Song Popularity:")
-    print(sp.playlist_items(playlist_id)["items"][0]["track"]["popularity"])
-    print("Artist/Artists:")
-    for artist in sp.playlist_items(playlist_id)["items"][0]["track"]["artists"]:
-        if artist != sp.playlist_items(playlist_id)["items"][0]["track"]["artists"][0]:
-            print(", ")
-        print (artist["name"])
-    print("Album:")
-    print(sp.playlist_items(playlist_id)["items"][0]["track"]["album"]["name"])
-    print("Album release date:")
-    print(sp.playlist_items(playlist_id)["items"][0]["track"]["album"]["release_date"])
-    # with open('/Users/duncan/Downloads/playlist_songs.csv', 'w') as f:
-    #     # create the csv writer
-    #     writer = csv.writer(f)
-    #     # write a header to the csv file
-    #     writer.writerow(csv_header)
-    #     # Loop for the body of the data
-    #     get_playlists(writer, 0)
+    with open('/Users/duncan/Downloads/playlist_data.csv', 'w') as f:
+        # create the csv writer
+        writer = csv.writer(f)
+        # write a header to the csv file
+        writer.writerow(csv_header)
+        # Loop for the body of the data
+        get_tracks(writer, 0)
