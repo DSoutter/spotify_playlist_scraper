54549102a4507481195b85d27d521992edd254c9
DSoutter <duncan.soutter@gmail.com>
2024-01-13 17:28:46 +0000

    working with loops to get all playlists

--- src/running_script.py
+++ src/running_script.py
@@ -2,6 +2,6 @@ import setup_script
 import playlist_logic as pl
 import tracks_logic as tl
 
-#pl.write_to_csv()
-tl.write_to_csv()
+pl.write_to_csv()
+# tl.write_to_csv()
 # Next read CSV and write a new csv for the tracks in a playlist
