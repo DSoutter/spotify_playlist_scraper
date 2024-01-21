57c71f4f13a229bcfd04bf62bc66e697aa50bd13
DSoutter <duncan.soutter@gmail.com>
2024-01-21 20:58:15 +0000

    added main function

--- src/running_script.py
+++ src/running_script.py
@@ -1,7 +1,10 @@
-import setup_script
+# import setup_script
 import playlist_logic as pl
 import tracks_logic as tl
 
-pl.write_to_csv()
-tl.write_to_csv()
-# Next read CSV and write a new csv for the tracks in a playlist
+def main():
+    pl.write_to_csv()
+    # tl.write_to_csv()
+
+if __name__ == "__main__":
+    main()
