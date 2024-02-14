8a2045d800f3398d8ab0422e6d404c508b0bdc81
DSoutter <duncan.soutter@gmail.com>
2024-02-14 21:32:24 +0000

    Corrected variable error

--- src/setup_script.py
+++ src/setup_script.py
@@ -5,6 +5,6 @@ CLIENT_ID = input("Enter Client_ID: ")
 CLIENT_SECRET = input("Enter Client_Secret: ")
 REDIRECT_URI = input("Enter Redirect URI: ")
 PATH = input("Enter the folder location you want to use, such as '/Users/duncan/Downloads': ").rstrip('/') + '/playlists.csv'
-DATAPATH = scope = "playlist-read-private"
+scope = "playlist-read-private"
 auth_manager = SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope=scope)
 sp_oauth = spotipy.Spotify(auth_manager=auth_manager)
