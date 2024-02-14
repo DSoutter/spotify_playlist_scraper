90334ed9aa3996f8804b8dc5d776ce6e31a07d7b
DSoutter <duncan.soutter@gmail.com>
2024-02-14 21:07:41 +0000

    Simplified setup

--- src/running_script.py
+++ src/running_script.py
@@ -1,10 +1,10 @@
-# import setup_script
+import setup_script as ss
 import playlist_logic as pl
 import tracks_logic as tl
 
 def main():
-    pl.write_to_csv()
-    # tl.write_to_csv()
+    pl.write_to_csv(ss.PATH)
+    tl.write_to_csv(ss.PATH)
 
 if __name__ == "__main__":
     main()
--- src/setup_script.py
+++ src/setup_script.py
@@ -1,27 +1,10 @@
-import os.path
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth  
 
-# Check if there's a spotify_credentials.py file
-file_exists = input("Do you have a spotify_credentials.py file? (Y/N): ")
-while file_exists.upper() not in ("Y", "N"):
-    file_exists = input("You did not enter \"Y\" or \"N\", try again: ")
-
-if file_exists.upper() == "Y":
-    try:
-        import spotify_credentials as creds
-        CLIENT_ID = creds.CLIENT_ID
-        CLIENT_SECRET = creds.CLIENT_SECRET
-    except:
-        print("Check the file is called \"spotify_credentials.py\" and is stored in the src folder.")
-        print("There should be two variables \"CLIENT_ID\" and \"CLIENT_SECRET\"")
-
-elif file_exists.upper() == "N":
-    CLIENT_ID = input("Enter Client_ID: ")
-    CLIENT_SECRET = input("Enter Client_Secret: ")
-
-REDIRECT_URI = "https://localhost:3000"
-
-scope = "playlist-read-private"
+CLIENT_ID = input("Enter Client_ID: ")
+CLIENT_SECRET = input("Enter Client_Secret: ")
+REDIRECT_URI = input("Enter Redirect URI: ")
+PATH = input("Enter the folder location you want to use, such as '/Users/duncan/Downloads': ").rstrip('/') + '/playlists.csv'
+DATAPATH = scope = "playlist-read-private"
 auth_manager = SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope=scope)
 sp_oauth = spotipy.Spotify(auth_manager=auth_manager)
